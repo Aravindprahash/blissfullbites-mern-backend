b3fa6aaac6fc8cbdc56e9272595ae3fbf616f133
Aravindprahash <aravindprahash19072004@gmail.com>
2025-08-15 21:29:03 +0530

    changes made commit

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,6 +15,18 @@ exports.getAll = async (req, res) => {
   }
 };
 
+exports.getById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 exports.create = async (req, res) => {
   try {
   
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,6 +3,7 @@ const product = require('../controllers/product.controller');
 const { verifyToken, isAdmin } = require('../middleware/auth');
 
 router.get('/', product.getAll);
+router.get('/:id', product.getById);
 router.post('/', verifyToken, isAdmin, product.create);
 router.put('/:id', verifyToken, isAdmin, product.update);
 router.delete('/:id', verifyToken, isAdmin, product.remove);
